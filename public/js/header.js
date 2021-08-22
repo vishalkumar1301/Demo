43a22e36ec094350fdd60b45953797f0ee87f0ae
Vishal Kumar <vishakumar@BLR.ALLEGISINDIA.COM>
2021-08-22 22:22:54 +0530

    added token on screen

--- public/js/header.js
+++ public/js/header.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+
+    let token = window.localStorage.getItem('x-auth-token');
+    console.log('x-auth-token', token);
+    $('.result').text('x-auth-token: ' + token);
+
     $('#logout').on('click', function() {
         window.localStorage.setItem('x-auth-token', '')
         window.location.href = window.location.origin + '/signin';
